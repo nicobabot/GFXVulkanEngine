3a791583d3b03b2fd015b60f7f2d7846b5dc40a0
nicobabot <nico.babotaltisen@gmail.com>
2023-12-01 14:24:54 +0100

    Added cube to project instead of quad

--- gfxMaths.h
+++ gfxMaths.h
@@ -6,7 +6,7 @@
 
 struct Vertex
 {
-	glm::vec2 position;
+	glm::vec3 position;
 	glm::vec3 color;
 
 	static VkVertexInputBindingDescription GetBindingDesctiption()
@@ -23,7 +23,7 @@ struct Vertex
 		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
 		attributeDescriptions[0].binding = 0;
 		attributeDescriptions[0].location = 0;
-		attributeDescriptions[0].format = VkFormat::VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].format = VkFormat::VK_FORMAT_R32G32B32_SFLOAT;
 		attributeDescriptions[0].offset = offsetof(Vertex, position);
 
 		attributeDescriptions[1].binding = 0;
@@ -36,7 +36,9 @@ struct Vertex
 
 };
 
-const std::vector<Vertex> vertices = 
+/*
+-Quad-
+const std::vector<Vertex> vertices =
 {
 	{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
 	{{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
@@ -47,6 +49,37 @@ const std::vector<Vertex> vertices =
 const std::vector<uint16_t> indices =
 {
 	{0, 1, 2, 2, 3, 0}
+};*/
+
+const std::vector<Vertex> vertices =
+{
+	{{0.5, -0.5, -0.5}, {1.0f,1.0f,1.0f}},
+	{{0.5, -0.5, 0.5}, {1.0f,1.0f,1.0f}},
+	{{-0.5, -0.5, 0.5}, {1.0f,1.0f,1.0f}},
+	{{-0.5, -0.5, -0.5}, {1.0f,1.0f,1.0f}},
+	{{0.5, 0.5, -0.5}, {1.0f,1.0f,1.0f}},
+	{{-0.5, 0.5, -0.5}, {1.0f,1.0f,1.0f}},
+	{{0.5, 0.5, 0.5}, {1.0f,1.0f,1.0f}},
+	{{-0.5, 0.5, 0.5}, {1.0f,1.0f,1.0f}}
+};
+
+const std::vector<uint16_t> indices =
+{
+	{
+		//Front
+		0,1,2,2,3,0,
+		//Back
+		//4,6,7,7,5,4,
+		5,7,6,6,4,5,
+		//Up
+		1,6,7,7,2,1,
+		//Down
+		0,4,5,5,3,0,
+		//Right
+		4,6,1,1,0,4,
+		//Left
+		3,2,7,7,5,3
+	}
 };
 
 struct UniformBufferObject 
